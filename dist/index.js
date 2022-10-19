17ff09677db7983b0899fc2361d3cfaae3fcb7f2
Robin Wieruch <hello@rwieruch.com>
2022-10-19 10:24:56 +0200

    feat(minimal): TS + Node

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const sayHello = (subject) => {
+    console.log('Hello ' + subject);
+};
+sayHello('TypeScript');
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHello = (subject: string): void => {
+  console.log('Hello ' + subject);
+};
+
+sayHello('TypeScript');
